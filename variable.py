864f40fc56395119a5a69e541a169712c8a48b06
sidak <spssachdeva21695@gmail.com>
2018-05-15 14:29:39 +0200

    Remove print statement, and rely on variable.data for shape

--- variable.py
+++ variable.py
@@ -4,9 +4,9 @@ class Variable():
 
 	def __init__(self, x):
 		super(Variable, self).__init__()
-		print(x)
 		self.data = x
 		self.grad = Tensor(x.shape)
+		self.requires_grad = True
 
 	def __repr__(self):
 		return "nnlib Variable containing \n" + self.data.__repr__()
@@ -14,8 +14,12 @@ class Variable():
 	def __str__(self):
 		return "nnlib Variable containing \n" + self.data.__str__()
 
+	def shape(self):
+		raise NotImplementedError
+		
 if __name__ == '__main__':
 	x = Tensor([1, 2, 3])
 	var = Variable(x)
 	print(var)
+	print(var.data.shape)
 	print(var.grad)
