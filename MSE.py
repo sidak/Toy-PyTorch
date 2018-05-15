e21282200e0948f867deb446cc7fbf4bd640d55f
wodw0 <nawel.naas@gmail.com>
2018-05-15 15:55:54 +0200

    backward mse

--- MSE.py
+++ MSE.py
@@ -8,8 +8,16 @@ class MSE(Module):
 		super(MSE, self).__init__()
 
 	def forward(self, pred, y):	 
-		return (pred.data - y.data).pow(2).sum()/2
+		return (pred.data - y.data).pow(2).sum()/y.shape[0]
 
-	def backward():
-		raise NotImplementedError
+	def backward(self, pred, y):
+		return 2*(pred.data - y.data).sum()/y.shape[0]
 
+if __name__ == '__main__':
+	x = Variable(Tensor([1, 2, 3]))
+	y = Variable(Tensor([8,2]))
+	net = Linear(3,2)
+	pred = Variable(Tensor([0,1]))
+	lossfunc = MSE()
+	out = lossfunc.forward(pred,y)
+	print(out)
