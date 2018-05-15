5e90add4cee1e19941253221aac4d923ca2f8b90
wodw0 <nawel.naas@gmail.com>
2018-05-15 15:44:43 +0200

    fixed mse forward

--- MSE.py
+++ MSE.py
@@ -1,14 +1,15 @@
 from module import Module
+from variable import Variable
 from torch import Tensor
+from linear import Linear
 
 class MSE(Module):
 	def __init__(self):
 		super(MSE, self).__init__()
 
-	def forward(self, pred, y):
-		diff = pred.data - y.data 		
-		loss = (diff.view(-1,1) @ diff)/2
-		return loss
+	def forward(self, pred, y):	 
+		return (pred.data - y.data).pow(2).sum()/2
 
 	def backward():
 		raise NotImplementedError
+
