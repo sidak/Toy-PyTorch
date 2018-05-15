89e683a5a10b5c6dfb260c38257448a16f9a555d
sidak <spssachdeva21695@gmail.com>
2018-05-15 15:02:53 +0200

    Pass tensor while initing variable and use data

--- linear.py
+++ linear.py
@@ -1,18 +1,18 @@
 from module import Module
 from variable import Variable
-
+from torch import Tensor
 class Linear(Module):
 
 	def __init__(self, in_dim, out_dim):
 		super(Linear, self).__init__()
 		self.in_dim = in_dim
 		self.out_dim = out_dim
-		self.weight = Variable((out_dim, in_dim))
-		self.bias = Variable((out_dim, ))
+		self.weight = Variable(Tensor((out_dim, in_dim)))
+		self.bias = Variable(Tensor((out_dim, )))
 		
 	def forward(self , input):
-		assert input.data.shape == (in_dim, )
-		return self.weight @ input + self.bias	
+		assert input.data.shape == (self.in_dim, )
+		return self.weight.data @ input.data + self.bias	
 	
 	def backward(self , * gradwrtoutput):
 		raise NotImplementedError
