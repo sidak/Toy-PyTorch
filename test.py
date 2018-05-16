6217e3c5446022d5f20dbe19a7f58db833db9604
sidak <spssachdeva21695@gmail.com>
2018-05-16 19:46:10 +0200

    udpate test

--- test.py
+++ test.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
 	x = Variable(Tensor([1, 2, 3]))
 	y = Variable(Tensor([7]))
 	print(x.shape, y.shape)
+
 	linear = Linear(x.shape[0], y.shape[0])
 	pred = linear.forward(x)
 
-	#loss = MSE(linear, y)
 
+	
 	#loss.backward()
 	print("Pred is ")
 	print(pred)
