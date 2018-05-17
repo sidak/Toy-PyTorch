ff1494fbb7e00671cbdaf9626dcfb585c320b593
sidak <spssachdeva21695@gmail.com>
2018-05-17 16:35:38 +0200

    test forward pass for 2 layer net without activations

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from variable import Variable
 from linear import Linear
 from torch import Tensor
+from network import Network
 #from mse import MSE
 
 
@@ -10,8 +11,10 @@ if __name__ == '__main__':
 	y = Variable(Tensor([7]))
 	print(x.shape, y.shape)
 
-	linear = Linear(x.shape[0], y.shape[0])
-	pred = linear.forward(x)
+	linear = Linear(x.shape[0], y.shape[0], weight_init='ones')
+	net = Network([linear])
+
+	pred = net.forward(x)
 
 
 	
@@ -19,3 +22,16 @@ if __name__ == '__main__':
 	print("Pred is ")
 	print(pred)
 	#print(x.grad)
+
+	linear1 = Linear(x.shape[0], x.shape[0], weight_init='ones')
+	linear2 = Linear(x.shape[0], y.shape[0], weight_init='ones')
+	
+	net_2layer = Network([linear1, linear2])
+
+	pred_2layer = net_2layer.forward(x)
+
+
+	
+	#loss.backward()
+	print("pred_2layer is ")
+	print(pred_2layer)
