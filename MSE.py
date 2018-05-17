Correct bug in MSE gradient

MSE.py
@@ -14,10 +14,10 @@ class MSE(Module):
 		# dim = 0 is necessary, otherwise returns just a scalar
 		self.pred = pred
 		self.y = y
-		return (pred.data - y.data).pow(2).sum(dim=0)/y.shape[0]
+		return Variable((pred.data - y.data).pow(2).sum(dim=0)/y.shape[0])
 
 	def backward(self):
-		return 2*(self.pred.data - self.y.data).sum(dim=0)/self.y.shape[0]
+		return Variable(2*(self.pred.data - self.y.data)/self.y.shape[0])
 
 if __name__ == '__main__':
 	x = Variable(Tensor([1, 2, 3]))