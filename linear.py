2b6c2ce3a0e1dfc3b8677cc69ad85004ccb82e5f
sidak <spssachdeva21695@gmail.com>
2018-05-17 16:35:17 +0200

    Init bias to zero by default

--- linear.py
+++ linear.py
@@ -4,18 +4,24 @@ from torch import Tensor
 
 class Linear(Module):
 
-	def __init__(self, in_dim, out_dim, weight_init=None):
+	def __init__(self, in_dim, out_dim, weight_init=None, bias_init='zero'):
 		super(Linear, self).__init__()
 		self.in_dim = in_dim
 		self.out_dim = out_dim
-		self.bias = Variable(Tensor(out_dim, ))
 		self.weight_init = weight_init
+		self.bias_init = bias_init
 		
 		if self.weight_init is None:
 			self.weight = Variable(Tensor(out_dim, in_dim))
 		elif self.weight_init == 'ones':
 			ones_list = [1] * (in_dim*out_dim)
 			self.weight = Variable(Tensor(ones_list).view(out_dim, in_dim))
+
+		if self.bias_init is None:
+			self.bias = Variable(Tensor(out_dim, ))
+		elif self.bias_init == 'zero':
+			zeros_list = [0] * (out_dim)
+			self.bias = Variable(Tensor(zeros_list).view(out_dim, ))
 
 	def forward(self , input):
 
