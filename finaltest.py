e8b2e8279cfb5d9c0ed7fdaa4822419a7c614c08
sidak <spssachdeva21695@gmail.com>
2018-05-17 20:20:48 +0200

    update finaltest

--- finaltest.py
+++ finaltest.py
@@ -1,26 +1,67 @@
 from torch import Tensor
 import math
-
+from linear import Linear
+from torch import Tensor
+from network import Network
+from MSE import MSE
+import matplotlib
+import matplotlib.pyplot as plt
+from activation import Relu, Tanh
 
 def generate_disc_set(nb):
     input = Tensor(nb, 2).uniform_(0, 1)
-    print(input.pow(2).sum(1).sub(1/ (2*math.pi)).sign())
-    target = input.pow(2).sum(1).sub(1/ (2*math.pi)).sign().mul(-1).add(1).div(2).long()
+    modified_input = input - 0.5
+    #print(modified_input.pow(2).sum(1).sub(1/ (2*math.pi)).sign().mul(-1))
+    #print("Modified inputs are ", modified_input)
+    target = modified_input.pow(2).sum(dim=1).sub(1/ (2*math.pi)).sign().mul(-1).add(1).div(2).long()
     return input, target
 
-train_input, train_target = generate_disc_set(10)
-#test_input, test_target = generate_disc_set(1000)
+train_input, train_target = generate_disc_set(1000)
+test_input, test_target = generate_disc_set(1000)
 
+num_hidden = 3
 
-print(train_input, train_target)
-'''
-mean, std = train_input.mean(), train_input.std()
+layers = []
+linear = Linear(2, 25, weight_init='ones')
+layers.append(linear)
+layers.append(Relu())
+for i in range(num_hidden-1):
+	layers.append(Linear(25, 25, weight_init='ones'))
+	layers.append(Relu())
+layers.append(Linear(25, 2, weight_init='ones'))
 
-train_input.sub_(mean).div_(std)
-test_input.sub_(mean).div_(std)
+net_2layer = Network(layers)
 
-train_input, train_target = Variable(train_input), Variable(train_target)
-test_input, test_target = Variable(test_input), Variable(test_target)
+mse = MSE()
 
-mini_batch_size = 100
-'''
+lr = 1e-3
+num_iter = 200
+
+timesteps = []
+loss_at_timesteps = []
+
+for it in range(num_iter):
+	
+	net_2layer.zero_grad()
+	pred_2layer = net_2layer.forward(train_input.narrow(0, 0, train_input.shape[0]))
+	loss = mse.forward(pred_2layer,test_input.narrow(0, 0, train_input.shape[0]) )
+	print("At iteration ", str(it), " the loss is ", loss)
+	loss_grad = mse.backward()
+	net_2layer.backward(loss_grad)
+	net_2layer.grad_step(lr=1e-3)
+
+	timesteps.append(it)
+	loss_at_timesteps.append(loss)
+
+print("Prediction at the end ", net_2layer.forward(x))
+
+fig, ax = plt.subplots()
+ax.plot(timesteps, loss_at_timesteps)
+
+ax.set(xlabel='iteration (s)', ylabel='Training Loss',
+       title='The Loss curve')
+ax.grid()
+
+#fig.savefig("test.png")
+plt.show()
+
