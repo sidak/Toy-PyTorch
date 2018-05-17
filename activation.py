6875f211bebcc6e1b5309defbe5630da6830d488
sidak <spssachdeva21695@gmail.com>
2018-05-17 19:55:35 +0200

    correct formulation of activation

--- activation.py
+++ activation.py
@@ -5,35 +5,35 @@ class Tanh(Module):
 
 	def __init__(self):
 		super(Tanh, self).__init__()
-		self.output = None
+		self.input = None
 
 	def forward(self, input):
-		self.output = self._tanh(input)
-		return self.output
+		self.input = input
+		return self._tanh(input)
 
 	def _tanh(self, x):
 		return 1.0 - (2.0/ (1.0 + (2.0*x).exp()))
 
-	def backward(self, gradwrtoutput):
-		#self.bias.grad = gradwrtoutput
-		#self.weight.grad = gradwrtoutput @ 
-		return gradwrtoutput * (1.0 - (self._tanh(self.output) * self._tanh(self.output)))
+	def backward(self, gradwrtinput):
+		#self.bias.grad = gradwrtinput
+		#self.weight.grad = gradwrtinput @ 
+		return gradwrtinput * (1.0 - (self._tanh(self.input) * self._tanh(self.input)))
 	
 
 class Relu(Module):
 
 	def __init__(self):
 		super(Relu, self).__init__()
-		self.output = None
+		self.input = None
 
 	def forward(self , input):
-		self.output = self._relu(input)
-		return self.output
+		self.input = input
+		return self._relu(input)
 
 	def _relu(self, x):
 		return (x.abs() + x)/2.0
 
-	def backward(self , gradwrtoutput):
-		#self.bias.grad = gradwrtoutput
-		#self.weight.grad = gradwrtoutput @ 
-		return gradwrtoutput * self._relu(self.output)
+	def backward(self , gradwrtinput):
+		#self.bias.grad = gradwrtinput
+		#self.weight.grad = gradwrtinput @ 
+		return gradwrtinput * self._relu(self.input)
