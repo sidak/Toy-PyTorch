ac71c2da4f1c1ed6b88d8782867e8cd265860cce
sidak <spssachdeva21695@gmail.com>
2018-05-17 18:20:49 +0200

    Correct bug in _get_components and update zero_grad

--- network.py
+++ network.py
@@ -26,19 +26,20 @@ class Network(Module):
 		for layer in reversed(self.layers):
 			grad = layer.backward(grad)
 
-	def _get_components(tens_shape):
+	def _get_components(self, tens_shape):
 		components = 1
 		for dm in tens_shape:
 			components *= dm
-		return dm
+		return components
 
-	def _set_zero(tens):
+	def _set_zero(self, tens):
 		tens_shape = tens.shape
-		zeros_list = [0] * _get_components(tens_shape)
+		#print(tens_shape)
+		zeros_list = [0.0] * self._get_components(tens_shape)
+		#print(zeros_list)
 		tens = Tensor(zeros_list).view(tens_shape)
 		return tens
 
 	def zero_grad(self):
-		for layer in layers:
-			for par_grad in layer.param_grad():
-				par_grad = _set_zero(par_grad)
+		for layer in self.layers:
+			layer.set_param_grad([self._set_zero(par_grad) for par_grad in layer.param_grad()])
