9ed30946c599b1f1c4bcfbed4d88d4443b7656e4
sidak <spssachdeva21695@gmail.com>
2018-05-17 19:40:50 +0200

    correct bugs in activations

--- activation.py
+++ activation.py
@@ -3,15 +3,15 @@ from torch import Tensor
 
 class Tanh(Module):
 
-	def __init__(self, layers):
+	def __init__(self):
 		super(Tanh, self).__init__()
 		self.output = None
 
 	def forward(self, input):
-		self.output = _tanh(input)
+		self.output = self._tanh(input)
 		return self.output
 
-	def _tanh(x):
+	def _tanh(self, x):
 		return 1.0 - (2.0/ (1.0 + (2.0*x).exp()))
 
 	def backward(self, gradwrtoutput):
@@ -22,18 +22,18 @@ class Tanh(Module):
 
 class Relu(Module):
 
-	def __init__(self, layers):
+	def __init__(self):
 		super(Relu, self).__init__()
 		self.output = None
 
 	def forward(self , input):
-		self.output = _relu(x)
+		self.output = self._relu(input)
 		return self.output
 
-	def _relu(x):
+	def _relu(self, x):
 		return (x.abs() + x)/2.0
 
 	def backward(self , gradwrtoutput):
 		#self.bias.grad = gradwrtoutput
 		#self.weight.grad = gradwrtoutput @ 
-		return gradwrtoutput * _relu(self.output)
+		return gradwrtoutput * self._relu(self.output)
