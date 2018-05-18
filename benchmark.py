55256c7e63aa8208e2d450b76f70309398b94e5e
Ignacio Aleman <ignaciosaleman@gmail.com>
2018-05-18 15:34:06 +0200

    benchmark updated to corret model and added loss printing

--- benchmark.py
+++ benchmark.py
@@ -6,7 +6,6 @@ import math
 
 import matplotlib
 import matplotlib.pyplot as plt
-from activation import Relu, Tanh
 
 def generate_disc_set(nb):
     input = Tensor(nb, 2).uniform_(0, 1)
@@ -32,6 +31,13 @@ def predict(net, input):
 
     return pred_max
 
+def plot_points(points, labels):
+    colors = ["red", "blue"]
+    for i in range(labels.shape[0]):
+        c = colors[labels[i]]
+        plt.plot(points[i][0], points[i][1], color=c, marker='o')
+    plt.show()
+
 
 batch_size = 1000
 train_input, train_target = generate_disc_set(batch_size)
@@ -40,15 +46,16 @@ test_input, test_target = generate_disc_set(batch_size)
 train_label = conv_to_one_hot(train_target)
 test_label = conv_to_one_hot(test_target)
 
+#plot_points(train_input, train_target)
 
 D_in = 2
-l1 = 100
-l2 = 200
-l3 = 50
+l1 = 25
+l2 = 25
+l3 = 25
 D_out = 2
 
-lr_rate = 1e-3
-epochs = 100
+lr_rate = 0.1
+epochs = 2000
 
 net = nn.Sequential(
         nn.Linear(D_in, l1),
@@ -58,17 +65,16 @@ net = nn.Sequential(
         nn.Linear(l2, l3),
         nn.ReLU(),
         nn.Linear(l3, D_out),
-        nn.ReLU()
+        nn.Tanh()
     )
 
 
 criterion = nn.MSELoss()
 optimizer = optim.SGD(net.parameters(), lr=lr_rate)
 
+train_loss = []
 
 for epoch in range(epochs):  # loop over the dataset multiple times
-
-    running_loss = 0.0
 
     # zero the parameter gradients
     optimizer.zero_grad()
@@ -81,13 +87,28 @@ for epoch in range(epochs):  # loop over the dataset multiple times
 
     # print statistics
     print('loss = {}'.format(loss.item()))
+    train_loss.append(loss.item())
 
 print('Finished Training')
 
+# Print number of errors
 pred_max = predict(net, train_input)
 nb_errors = compute_nb_errors(pred_max, train_target)
 print('Number of train errors = {} out of {}'.format(nb_errors, batch_size))
+#plot_points(train_input, pred_max)
 
 pred_max = predict(net, test_input)
 nb_errors = compute_nb_errors(pred_max, test_target)
 print('Number of test errors = {} out of {}'.format(nb_errors, batch_size))
+
+# Plot and save loss
+fig, ax = plt.subplots()
+ax.plot(train_loss)
+
+ax.set(xlabel='iteration (s)', ylabel='Training Loss',
+    title='The Loss curve for {} epochs'.format(epochs))
+ax.grid()
+
+fig.savefig("loss_epochs={}.png".format(epochs))
+plt.show()
+
