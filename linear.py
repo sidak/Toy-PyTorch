30a2478cd003ed82d8a6caf3944934dc98dd4fc8
sidak <spssachdeva21695@gmail.com>
2018-05-18 11:05:44 +0200

    We did a silly mistake, bias just depends on the number of nodes in your layer

--- linear.py
+++ linear.py
@@ -20,14 +20,36 @@ class Linear(Module):
 		elif self.weight_init == 'ones':
 			ones_list = [1] * (self.in_dim*self.out_dim)
 			self.weight = Tensor(ones_list).view(self.out_dim, self.in_dim)
-		self.weight_grad = Tensor(self.out_dim, self.in_dim)
+		elif self.weight_init == 'uniform_pos_neg':
+			self.weight = Tensor(self.out_dim, self.in_dim).uniform_(-1, 1)
+		elif self.weight_init == 'uniform_non_neg':
+			self.weight = Tensor(self.out_dim, self.in_dim).uniform_(0, 1)
 
+		self.weight_grad = Tensor(self.out_dim, self.in_dim)
+		'''
 		if self.bias_init is None:
 			self.bias = Tensor(self.out_dim, nb_samples)
 		elif self.bias_init == 'zero':
 			zeros_list = [0] * (self.out_dim * nb_samples)
 			self.bias = Tensor(zeros_list).view(self.out_dim, nb_samples)
+		elif self.bias_init == 'uniform_pos_neg':
+			self.bias = Tensor(self.out_dim, nb_samples).uniform_(-1, 1)
+		elif self.bias_init == 'uniform_non_neg':
+			self.bias = Tensor(self.out_dim, nb_samples).uniform_(0, 1)
+
 		self.bias_grad = Tensor(self.out_dim, nb_samples)
+		'''
+		if self.bias_init is None:
+			self.bias = Tensor(self.out_dim, 1)
+		elif self.bias_init == 'zero':
+			zeros_list = [0] * (self.out_dim * 1)
+			self.bias = Tensor(zeros_list).view(self.out_dim, 1)
+		elif self.bias_init == 'uniform_pos_neg':
+			self.bias = Tensor(self.out_dim, 1).uniform_(-1, 1)
+		elif self.bias_init == 'uniform_non_neg':
+			self.bias = Tensor(self.out_dim, 1).uniform_(0, 1)
+
+		self.bias_grad = Tensor(self.out_dim, 1)
 		
 	def forward(self , input):
 		self.input = input
@@ -37,12 +59,20 @@ class Linear(Module):
 		# 
 		# input = (in, 1) bias (out, 1)
 		# input = (in, 2) bias (out, 2)
-		return self.weight @ input + self.bias
+		#print("shape of bias is ", self.bias.shape)
+		#print("shape of weight is ", self.weight.shape)
+		#print("shape of input is ", self.input.shape)
+		#print("shape of bias expanded is ", self.bias.repeat(1, input.shape[1]).shape)
+
+		#print("shape of wt @ input  is ", (self.weight @ input).shape)
+		
+		return self.weight @ input + self.bias.repeat(1, input.shape[1])
 	
 	def backward(self , gradwrtoutput):
 		# init them to zero in the loop as params.zero_grad()
 		# Add then here I should accumulate
-		self.bias_grad += gradwrtoutput
+		#print("shape of gradwrtoutput", gradwrtoutput.sum(dim=1).shape)
+		self.bias_grad += gradwrtoutput.sum(dim=1)
 		nb_samples = gradwrtoutput.shape[1]
 		self.weight_grad +=  gradwrtoutput.view(-1, nb_samples) @ self.input.view(nb_samples, -1)
 		return self.weight.t() @ gradwrtoutput
@@ -59,5 +89,5 @@ class Linear(Module):
 
 	def update_param(self, lr):
 		self.weight = self.weight - lr*self.weight_grad
-		self.bias = self.bias - lr*self.bias_grad
+		#self.bias = self.bias - lr*self.bias_grad
 
